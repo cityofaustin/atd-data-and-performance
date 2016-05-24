80849e0475f1ea54b54e00da3cdb4ee6c9497394
John Clary <john.clary@austintexas.gov>
2016-05-24 15:51:08 -0500

    update default map zoom

--- components/scripts/intersection-status-map.js
+++ components/scripts/intersection-status-map.js
@@ -1,5 +1,5 @@
 
-var map = new L.Map("map", {center : [30.28, -97.735], zoom : 11, minZoom : 1, maxZoom : 20});      // make a map
+var map = new L.Map("map", {center : [30.28, -97.735], zoom : 12, minZoom : 1, maxZoom : 20});      // make a map
         
 var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
     attribution : 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
