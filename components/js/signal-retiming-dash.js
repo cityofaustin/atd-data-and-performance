752435d45cb97d47accd26876cd807d98d57f738
johnclary <john.clary@austintexas.gov>
2016-08-11 17:29:29 -0400

    a humble yet beautiful pie

--- components/js/signal-retiming-dash.js
+++ components/js/signal-retiming-dash.js
@@ -1,4 +1,4 @@
-var annual_goals = {
+var ANNUAL_GOALS = {
     "2016" : {
         retime_goal: 200,
         avg_reduction_goal: .05    
@@ -11,9 +11,9 @@ var annual_goals = {
 
 var selected_year = "2016";
 
-var retime_goal = +annual_goals[selected_year]["retime_goal"];
+var retime_goal = +ANNUAL_GOALS[selected_year]["retime_goal"];
 
-var reduction_goal = +annual_goals[selected_year]["avg_reduction_goal"];
+var reduction_goal = +ANNUAL_GOALS[selected_year]["avg_reduction_goal"];
 
 var data_url = "../components/data/intersection_status_snapshot.json";
 
@@ -46,15 +46,13 @@ d3.json(data_url, function(dataset) {
 
     populateStat("info-1", retime_current);
 
-    populateStat("info-2", avg_reduction); 
+    createPieChart("info-2", dataset);
 
 });
 
 d3.select("#year-selector").on("change", function(d){
     
     populateStat("info-1", 300);
-
-    populateStat("info-2", 100);
 
 })
 
@@ -63,10 +61,6 @@ function getSelectedYear(){
     var selected_year = d3.select("#year-selector").select("option").attr("value");
     
     return selected_year;
-
-}
-
-function drawChart() {
 
 }
 
@@ -92,7 +86,60 @@ function populateStat(divId, statValue) {
 
 
 
+function createPieChart(divId, dataset) {
 
+     poll_stats = d3.nest()
+            .key(function (d) { return d.pollstatus; })
+            .rollup(function (v) { return v.length; })
+            .map(dataset);
+
+     values = d3.values(poll_stats);  //  what's a better way to do this?
+
+     keys = d3.keys(poll_stats);  //  the challenge is accessing the keys for assigning classes
+
+
+    var width = 400;
+
+    var height = 300;
+
+    var radius = 150;
+
+    var COMM_TYPES = {
+        0: "no_comm",
+        1: "ok"
+    };
+
+     var pie = d3.layout.pie()  //  not d4 compatible
+            .sort(null)
+            .value(function (d) {
+               return d;
+            });
+
+    var arc = d3.svg.arc()
+        .outerRadius(radius)
+        .innerRadius(radius * .5);
+
+    var svg = d3.select("#" + divId).append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    var path1 = svg.datum(values).selectAll("path")
+        .data(pie)
+        .enter()
+        .append("g")
+        .attr("class", function(d, i) {
+            return COMM_TYPES[keys[i]] + " arc";
+        })
+        .attr("id", "pie")
+        .append("path")
+        .attr("d", arc)
+        .attr("stroke", "white")
+        .each(function (d) {
+            this._current = d;
+}); // store the current angles
+}
 
 
 
