3e706dcc3d492e6c998068398ab740a6f4f26959
johnclary <john.clary@austintexas.gov>
2016-11-20 09:54:59 -0600

    baby steps

--- components/js/operations.js
+++ components/js/operations.js
@@ -1,12 +1,21 @@
-var pizza;
 
 var map;
 
-
-var logfile_url = 'https://data.austintexas.gov/resource/n5kp-f8k4.json?$select=timestamp&$where=event=%27signal_status_update%27%20AND%20response_message%20IS%20NULL&$order=timestamp+DESC&$limit=1'
+//  var logfile_url = 'https://data.austintexas.gov/resource/n5kp-f8k4.json?$select=timestamp&$where=event=%27signal_status_update%27%20AND%20response_message%20IS%20NULL&$order=timestamp+DESC&$limit=1'
 
 //  var data_url = 'https://data.austintexas.gov/resource/d7s7-auiw.json';
 var data_url = '../components/data/fake_signal_data.json';
+
+var t2 = d3.transition()
+                .ease(d3.easeQuad)
+                .duration(500);
+
+var default_format = Math.round();
+
+var FORMAT_TYPES = {
+    'round': function(val) { return Math.round(val) }
+};
+
 
 
 //  init tooltips and touch detect
@@ -17,8 +26,8 @@ $(document).ready(function(){
     if (is_touch_device()) {
         
         d3.select('.map')
-            .style("margin-right", "10px")
-            .style("margin-left", "10px");
+            .style('margin-right', '10px')
+            .style('margin-left', '10px');
     }
 
     getRequestData(data_url);
@@ -29,20 +38,26 @@ $(document).ready(function(){
 
 function main(data){
 
-    pizza = data;
-
     makeMap();
 
-    populateTable(data);
-    // populateInfoStat(data, "info-1", function(){
+    populateTable(data, 'data_table');
 
-        //  infostat callback
+    populateInfoStat('signals', 0, 1050, t2, 'round', function(){
 
-    //});
+        populateInfoStat('phbs', 0, 43, t2, 'round', function(){
 
+            populateInfoStat('cameras', 0, 228, t2, 'round', function(){
 
-};
+                populateInfoStat('sensors', 0, 127, t2, 'round', function(){
+                 });
 
+            });
+
+        })
+
+    });
+
+}
 
 
 
@@ -52,7 +67,7 @@ function getRequestData(url) {
         'global' : false,
         'cache' : false,
         'url' : url,
-        'dataType' : "json",
+        'dataType' : 'json',
         'success' : function (data) {
             main(data);
         }
@@ -69,9 +84,9 @@ function getLogData(url) {
         'global' : false,
         'cache' : false,
         'url' : url,
-        'dataType' : "json",
+        'dataType' : 'json',
         'success' : function (data) {
-            postUpdateDate(data, "info-1");
+            postUpdateDate(data, 'info-1');
         }
     
     }); //end get data
@@ -80,20 +95,11 @@ function getLogData(url) {
 
 
 
-function is_touch_device() {  //  via https://ctrlq.org/code/19616-detect-touch-screen-javascript
-        return (('ontouchstart' in window)
-      || (navigator.MaxTouchPoints > 0)
-      || (navigator.msMaxTouchPoints > 0));
-}
-
-
-
-
 function makeMap(dataset) {
 
     L.Icon.Default.imagePath = '../components/images/';
 
-    map = new L.Map("map", {
+    map = new L.Map('map', {
         center : [30.28, -97.735],
         zoom : 10,
         minZoom : 1,
@@ -111,12 +117,14 @@ function makeMap(dataset) {
 }
 
 
-function populateTable(dataset) {
 
-    table = $('#data_table').DataTable({
+
+function populateTable(dataset, divId) {
+
+    table = $('#' + divId).DataTable({
         data: dataset,
-        "bLengthChange": false,
-        "bFilter": false,
+        'bLengthChange': false,
+        'bFilter': false,
         columns: [
 
             { data: 'signal_type' },
@@ -131,5 +139,61 @@ function populateTable(dataset) {
     })
 }
 
+
+
+
+function populateInfoStat(divId, old_value, new_value, transition, format_type, next) {
+
+    if (!format_type) {
+        var format_type = 'round';
+    }
+
+    d3.select('#' + divId)
+        .select('.info_text')
+        .text(FORMAT_TYPES[format_type](old_value))
+        .transition(transition)
+        .tween('text', function () {
+            
+            var that = d3.select(this);
+
+            var i = d3.interpolate(0, new_value);
+            
+            return function (t) {
+            
+                that.text( FORMAT_TYPES[format_type](i(t)) );
+            
+            }
+
+        })
+        .on("end", function(){
+
+            if (next) {
+                next();
+
+            } else {
+                return '';
+            }
+        });
+
+}
+
+
+
+
+
+
+
+
+function defaultFormat(val){
+    return Math.round(val);
+}
+
+
+
+function is_touch_device() {  //  via https://ctrlq.org/code/19616-detect-touch-screen-javascript
+        return (('ontouchstart' in window)
+      || (navigator.MaxTouchPoints > 0)
+      || (navigator.msMaxTouchPoints > 0));
+}
 
 
