aed0ddde4239512496c8e0266598627bdc950c75
johnclary <john.clary@austintexas.gov>
2016-11-26 00:08:42 -0600

    factor and refactor went for a walk...

--- components/js/operations.js
+++ components/js/operations.js
@@ -1,20 +1,53 @@
-var pizza;
+var t_options = {
+    ease : d3.easeQuad,
+    duration : 10000
+};
 
-var map;
-
-
-var signals_id = 'p53x-x73x';
-
-var t2 = d3.transition()
-                .ease(d3.easeQuad)
-                .duration(500);
-
-var default_format = Math.round();
-
-var FORMAT_TYPES = {
+var formats = {
     'round': function(val) { return Math.round(val) }
 };
 
+var assets = [
+    {
+        'name' : 'signals',
+        'init_val' : 0,
+        'resource_id' : 'p53x-x73x',
+        'format' : 'round',
+        'data' : 1050 //  use empty str and populate later
+    },
+    {
+        'name' : 'phbs',
+        'init_val' : 0,
+        'resource_id' : 'p53x-x73x',
+        'format' : 'round',
+        'data' : 43
+    },
+    {
+        'name' : 'cameras',
+        'init_val' : 0,
+        'resource_id' : 'p53x-x73x',
+        'format' : 'round',
+        'data' : 228
+    },
+    {
+        'name' : 'sensors',
+        'init_val' : 0,
+        'resource_id' : 'p53x-x73x',
+        'format' : 'round',
+        'data' : 127
+    }
+];
+
+
+
+
+var map_options = {
+        center : [30.28, -97.735],
+        zoom : 10,
+        minZoom : 1,
+        maxZoom : 20,
+        scrollWheelZoom: false
+    };
 
 
 $(document).ready(function(){
@@ -28,31 +61,43 @@ $(document).ready(function(){
             .style('margin-left', '10px');
     }
 
-    getOpenData(signals_id);
+    main();
 
 });
 
 
-function main(data){
 
-    makeMap();
+function main(){
 
-    populateTable(data, 'data_table');
+    //  getOpenData(signals_id);
 
-    populateInfoStat('signals', 0, 1050, t2, 'round', function(){
+    var map = makeMap('map', map_options);
 
-        populateInfoStat('phbs', 0, 43, t2, 'round', function(){
+    var infos = appendInfoText(assets);
 
-            populateInfoStat('cameras', 0, 228, t2, 'round', function(){
+    transitionInfoStat(infos, t_options);
 
-                populateInfoStat('sensors', 0, 127, t2, 'round', function(){
-                 });
+}
 
-            });
 
+function makeMap(divId, options) {
+
+    //  mappy map
+    L.Icon.Default.imagePath = '../components/images/';
+
+    var layers = {
+        Stamen_TonerLite: L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
+            attribution : 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+            subdomains : 'abcd',
+            maxZoom : 20,
+            ext : 'png'
         })
+    }
 
-    });
+    var map = new L.Map(divId, options)
+        .addLayer(layers['Stamen_TonerLite']);
+
+    return map;
 
 }
 
@@ -71,8 +116,6 @@ function getOpenData(resource_id) {
         'url' : url,
         'dataType' : 'json',
         'success' : function (data) {
-            pizza = data;
-            
             main(data);
         },
         error: function(jqXHR, textStatus, errorThrown) {
@@ -86,27 +129,18 @@ function getOpenData(resource_id) {
 
 
 
-function makeMap(dataset) {
+function appendInfoText(data) {
 
-    L.Icon.Default.imagePath = '../components/images/';
+    var selection = d3.selectAll('.info')
+        .data(data)
+        .append('text')
+        .text(function(d) {
+            return d.init_val;
+        });
 
-    map = new L.Map('map', {
-        center : [30.28, -97.735],
-        zoom : 10,
-        minZoom : 1,
-        maxZoom : 20,
-        scrollWheelZoom: false
-    });      // make a map
-
-    var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
-        attribution : 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-        subdomains : 'abcd',
-        maxZoom : 20,
-        ext : 'png'
-    }).addTo(map);
+    return selection;
 
 }
-
 
 
 
@@ -131,53 +165,33 @@ function populateTable(dataset, divId) {
 }
 
 
+function transitionInfoStat(selection, options) {
 
+    var t = d3.transition()
+        .ease(options.ease)
+        .duration(options.duration);
 
-function populateInfoStat(divId, old_value, new_value, transition, format_type, next) {
-
-    if (!format_type) {
-        var format_type = 'round';
-    }
-
-    d3.select('#' + divId)
-        .select('.info_text')
-        .text(FORMAT_TYPES[format_type](old_value))
-        .transition(transition)
+    selection.transition(t)  //  do this for each selection in sequence
         .tween('text', function () {
             
             var that = d3.select(this);
 
+            var new_value = that.data()[0].data;
+            
             var i = d3.interpolate(0, new_value);
             
             return function (t) {
             
-                that.text( FORMAT_TYPES[format_type](i(t)) );
+                that.text( formats['round'](i(t)) );  // how to access the format type from the 'this' data?
             
             }
 
-        })
-        .on("end", function(){
-
-            if (next) {
-                next();
-
-            } else {
-                return '';
-            }
         });
 
+    return selection;
+
 }
 
-
-
-
-
-
-
-
-function defaultFormat(val){
-    return Math.round(val);
-}
 
 
 
