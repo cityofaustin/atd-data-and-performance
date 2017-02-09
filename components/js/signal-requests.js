9b00ba010f844073e09e0292a64069c08422ad84
John Clary <john.clary@austintexas.gov>
2017-02-09 13:34:58 -0600

    marker styles

--- components/js/signal-requests.js
+++ components/js/signal-requests.js
@@ -1,9 +1,5 @@
 var map, feature_layer, data;
 
-<<<<<<< HEAD
-=======
-//  
->>>>>>> 0f2d8b5f9bd9750d5925cc5499f256bb8c038809
 var requests_url = '../components/data/fake_request_data.json';
 
 var t_options = {
@@ -31,7 +27,7 @@ var default_style = {
     'PHB': {
         color: '#fff',
         weight: 1,
-        fillColor: '#ff7f00',
+        fillColor: '#e28400',
         fillOpacity: .8
     },
     'TRAFFIC' : {
@@ -322,18 +318,13 @@ function createMarkers(data, style) {
         
         if (data[i].request_note) {
             var request_note = data[i].request_note;
-            console.log(request_note)
         }
 
         data[i]['marker'] = L.circle([lat,lon], 500)
-<<<<<<< HEAD
           .setStyle(style[eval_type])
-          .bindPopup( '<b><i class="fa ' + icon_lookup[eval_type] + '" ></i> ' + eval_type  + ' REQUEST </b></br>' + location_name + '</br> Status: ' + status + '</b></br> <i>' + request_note + '</i>')
-=======
+          .bindPopup( '<b><i class="fa ' + icon_lookup[eval_type] + '" ></i> ' + eval_type  + ' REQUEST </b></br>' + location_name + '</br> Status: ' + status + '</br> <i>' + request_note + '</i>')
           .setStyle(style)
-          .bindPopup( '<i class="fa ' + icon_lookup[type] + '" ></i> ' + type  + ' REQUEST </br><b>' + location_name + '</b></br> Status: ' + status + '</b></br> <i>' + request_note + '</i>')
-          .on('click', markerClick);
->>>>>>> 0f2d8b5f9bd9750d5925cc5499f256bb8c038809
+          .bindPopup( '<b><i class="fa ' + icon_lookup[eval_type] + '" ></i> ' + eval_type  + ' REQUEST </b></br>' + location_name + '</br> Status: ' + status + '</br> <i>' + request_note + '</i>')
 
     }
     
@@ -518,4 +509,3 @@ function filterUnique(dataset) {
     return unique_records;
 
 }
-
