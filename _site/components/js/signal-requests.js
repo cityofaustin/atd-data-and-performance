3bd3f754f369ad122c508107b298944bc23aed1d
johnclary <john.clary@austintexas.gov>
2017-02-25 12:46:38 -0600

    switch to cartodb basemap

--- _site/components/js/signal-requests.js
+++ _site/components/js/signal-requests.js
@@ -157,12 +157,11 @@ function makeMap(divId, options) {
     L.Icon.Default.imagePath = '../components/images/';
 
     var layers = {
-        Stamen_TonerLite: L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
-            attribution : 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-            subdomains : 'abcd',
-            maxZoom : 20,
-            ext : 'png'
-        })
+        Stamen_TonerLite: L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+    subdomains: 'abcd',
+    maxZoom: 19
+})
     }
 
     var map = new L.Map(divId, options)
