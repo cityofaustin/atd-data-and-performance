adb873d1d4119968348e0cdacf5703edd29f4910
johnclary <john.clary@austintexas.gov>
2017-03-23 23:27:33 -0500

    hide traffic cams (for now)

--- _site/components/js/signal-assets.js
+++ _site/components/js/signal-assets.js
@@ -1,12 +1,9 @@
 var quote_data
 
-
-
 var t_options = {
     ease : d3.easeQuad,
     duration : 500
 };
-
 
 var formats = {
     'round': function(val) { return Math.round(val) },
@@ -15,11 +12,7 @@ var formats = {
     'formatTime' : d3.timeFormat("%I:%M %p")
 };
 
-
-//  https://github.com/d3/d3-queue
 var q = d3.queue();
-
-
 
 var global_data = [
 
@@ -76,6 +69,7 @@ for (var i = 0; i < global_data.length; ++i) {
     }
 
 }
+
 
 
 function main(data) {
@@ -266,9 +260,9 @@ function postUpdateDate(selection, event, resource_id) {
 
     var data_url = 'https://data.austintexas.gov/resource/' + resource_id;
 
-    var logfile_url = 'https://data.austintexas.gov/resource/n5kp-f8k4.json?$select=timestamp&$where=event=%27_XXX_%27&$order=timestamp+DESC&$limit=1'
-
-    var logfile_url = logfile_url.replace('_XXX_', event);
+    var logfile_url = "https://data.austintexas.gov/resource/n5kp-f8k4.json?$query=SELECT * WHERE event='_XXX_' AND (created > 0 OR updated > 0 OR deleted > 0) ORDER BY timestamp DESC LIMIT 1";
+    
+    logfile_url = logfile_url.replace('_XXX_', event);
 
     d3.json(logfile_url, function(error, data) {
 
@@ -276,10 +270,10 @@ function postUpdateDate(selection, event, resource_id) {
 
         update_date = readableDate( update_date_time );
 
-        var update_time = formats.formatTime( update_date_time );
+        // var update_time = formats.formatTime( update_date_time );
 
         selection.append('h5')
-            .html("Updated " + update_date + " at " + update_time +
+            .html("Updated " + update_date +
                 " | <a href=" + data_url + " target='_blank'> Data <i  class='fa fa-download'></i> </a>"
              );
 
