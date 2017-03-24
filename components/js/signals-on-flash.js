940f7bdb98032591f9d69823fe9c7a700980bb29
John Clary <john.clary@austintexas.gov>
2017-03-24 10:58:59 -0500

    back to stamen toner

--- components/js/signals-on-flash.js
+++ components/js/signals-on-flash.js
@@ -304,6 +304,13 @@ function makeMap(dataset) {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
         subdomains: 'abcd',
         maxZoom: 19
+    });
+
+    var stamen_toner_lite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
+        attribution : 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        subdomains : 'abcd',
+        maxZoom : 20,
+        ext : 'png' 
     }).addTo(map);
 
     populateMap(map, dataset, function(){
