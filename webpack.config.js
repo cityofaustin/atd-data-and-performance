6b723b6c61160e5b0fd43816f6c5f595064b5e1a
Mateo Clarke <code@mateoclarke.com>
2018-12-13 11:37:36 -0600

    Add webpack to jekyll

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+
+module.exports = {
+  // webpack folder’s entry js — excluded from jekll’s build process.
+  entry: "./webpackjs/entry.js",
+  output: {
+    // we’re going to put the generated file in the assets folder so jekyll will grab it.
+    // if using GitHub Pages, use the following:
+    // path: "assets/javascripts"
+    // path: "./components/js/",
+    // filename: "bundle.js"
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "components/js")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.jpe?g$|\.ico$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
+        loader: "file-loader?name=[name].[ext]" // <-- retain original file name
+      }
+    ]
+  }
+};
---
+++ webpackjs/components/DocklessData.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+class DocklessData extends Component {
+  render() {
+    return <div>Hey, y’all!! 🤠</div>;
+  }
+}
+export default DocklessData;
