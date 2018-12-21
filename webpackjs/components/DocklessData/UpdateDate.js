c9ff8e44e40411d735a3f29d1341c1af701bd990
Mateo Clarke <code@mateoclarke.com>
2018-12-21 16:04:30 -0600

    pull updated_at from Socrata in some cases

--- webpackjs/components/DocklessData/UpdateDate.js
+++ webpackjs/components/DocklessData/UpdateDate.js
@@ -5,18 +5,20 @@ import { timeFormat } from "d3-time-format";
 class UpdateDate extends Component {
   constructor(props) {
     super(props);
+    const { resourceId, updateEvent } = this.props;
     this.state = {
       updateDate: null,
       updateTime: null
     };
-    this.jobsUrl = "https://transportation-data.austintexas.io/jobs";
-    this.downloadUrl = `https://data.austintexas.gov/resource/${
-      this.props.resourceId
-    }`;
-    if (this.props.updateEvent) {
-      this.jobsUrl = `${this.jobsUrl}?name=eq.${
-        this.props.updateEvent
-      }&status=eq.success&order=start_date.desc&limit=1`;
+    this.isATDScheduledJob = this.props.updateEvent ? true : false;
+    this.downloadUrl = `https://data.austintexas.gov/resource/${resourceId}`;
+
+    if (this.isATDScheduledJob) {
+      this.updatedAtUrl = `https://transportation-data.austintexas.io/jobs?name=eq.${updateEvent}&status=eq.success&order=start_date.desc&limit=1`;
+    } else {
+      // when there is no update event, use the Socrata Metadata API
+      // to look for an update timestamp
+      this.updatedAtUrl = `https://data.austintexas.gov/api/views/${resourceId}.json`;
     }
   }
 
@@ -32,15 +34,18 @@ class UpdateDate extends Component {
   }
 
   componentDidMount() {
-    axios.get(this.jobsUrl).then(res => {
-      if (res.data.length > 0) {
-        const updateDateTime = new Date(res.data[0].start_date);
-
-        this.setState({
-          updateDate: this.readableDate(updateDateTime),
-          updateTime: timeFormat("%x")(updateDateTime)
-        });
+    axios.get(this.updatedAtUrl).then(res => {
+      let updateDateTime;
+      if (res.data.length === 0) return false;
+      if (this.isATDScheduledJob) {
+        updateDateTime = new Date(res.data[0].start_date);
+      } else {
+        updateDateTime = new Date(res.data.rowsUpdatedAt * 1000);
       }
+      this.setState({
+        updateDate: this.readableDate(updateDateTime),
+        updateTime: timeFormat("%I:%M %p")(updateDateTime)
+      });
     });
   }
 
