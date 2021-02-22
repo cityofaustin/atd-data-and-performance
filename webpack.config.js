37e821bb50a0031fa0d7cb5889c929e347aa4bfc
John Clary <john.clary@austintexas.gov>
2021-02-22 15:02:27 -0600

    remove deprecated about.js config entry

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,7 @@ module.exports = {
   context: path.resolve(__dirname, "webpackjs"),
   // webpack folder’s entry js — excluded from jekll’s build process.
   entry: {
-    micromobility: "./micromobility-data.js",
-    about: "./about.js"
+    micromobility: "./micromobility-data.js"
   },
   output: {
     // we’re going to put the generated file in the assets folder so jekyll will grab it.
