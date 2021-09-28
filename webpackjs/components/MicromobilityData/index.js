96beb2b7728d21b9fb0342229ba10c741d34552e
John Clary <john.clary@austintexas.gov>
2021-09-27 19:10:28 -0500

    use new 311 endpoint and query to accomodate it

--- webpackjs/components/MicromobilityData/index.js
+++ webpackjs/components/MicromobilityData/index.js
@@ -12,6 +12,8 @@ import Description from "./Description";
 
 import "./react-calendar.css";
 
+const resourceId311 = `xwdj-i9he`;
+
 class MicromobilityData extends Component {
   constructor(props) {
     super(props);
@@ -54,7 +56,6 @@ class MicromobilityData extends Component {
     // Queries the SODA API with the current date range query and stores data in state.
     // "dateRangeType" tells UI how to display date range to user ("current," "month," "range" or "all time")
     const resourceId = `7d8e-dm7r`;
-    const resourceId311 = `5h38-fd8d`;
     const tripSelectors = `avg(trip_duration)/60 as avg_duration_minutes, sum(trip_distance) * 0.000621371 as total_miles, avg(trip_distance) * 0.000621371 as avg_miles, count(trip_id) as total_trips`;
     const tripFilters =  `trip_distance * 0.000621371 >= 0.1 AND trip_distance * 0.000621371 < 500 AND trip_duration < 86400`;
 
@@ -62,7 +63,7 @@ class MicromobilityData extends Component {
     const allModesQuery = `SELECT ${tripSelectors} WHERE start_time between ${dateQuery} AND ${tripFilters}`;
     const deviceCountScooterQuery = `SELECT DISTINCT device_id WHERE start_time between ${dateQuery} AND vehicle_type = 'scooter' LIMIT 1000000000`;
     const deviceCountBicycleQuery = `SELECT DISTINCT device_id WHERE start_time between ${dateQuery} AND vehicle_type = 'bicycle' LIMIT 1000000000`;
-    const threeOneOneQuery = `SELECT count(sr_type_code) as count WHERE sr_created_date between ${dateQuery} AND sr_type_code == "DOCKMOBI"`;
+    const threeOneOneQuery = `SELECT count(sr_type_desc) WHERE sr_created_date between ${dateQuery} AND (contains(upper(\`sr_type_desc\`), upper('dockless')))`;
 
     const dataByModeUrl = `https://data.austintexas.gov/resource/${resourceId}.json?$query=${dataByModeQuery}`;
     const allModesUrl = `https://data.austintexas.gov/resource/${resourceId}.json?$query=${allModesQuery}`;
@@ -360,10 +361,10 @@ class MicromobilityData extends Component {
                 title="311 Service Requests"
                 value={this.getValue(
                   "threeOneOneData",
-                  "count"
+                  "count_sr_type_desc"
                 )}
                 icon="phone"
-                resourceId={"5h38-fd8d"}
+                resourceId={resourceId311}
                 numberFormat="thousands"
               />
             </CardContainer>
