19a73c38dc0f76d0b0342da66ca9942ec3fc4083
John Clary <john.clary@austintexas.gov>
2022-07-09 23:26:07 -0400

    cleanup classes

--- components/FlexyInfo.js
+++ components/FlexyInfo.js
@@ -1,12 +1,8 @@
 export default function FlexyInfo({ label, value }) {
   return (
-    <div className="d-flex w-100 justify-content-between text-dts-dark-gray">
-      <p className="my-0">
-        <small>{label}</small>
-      </p>
-      <p className="my-0">
-        <small>{value}</small>
-      </p>
+    <div className="d-flex justify-content-between">
+      <span className="fw-bold">{label}</span>
+      <span>{value}</span>
     </div>
   );
 }
