543a40fc89097292a7eaf74995e16ad5796314d5
John Clary <john.clary@austintexas.gov>
2022-07-12 11:22:54 -0400

    make location name styles match b/t popups

--- components/pages/cameras/PopUpHoverContent.js
+++ components/pages/cameras/PopUpHoverContent.js
@@ -3,7 +3,7 @@ import { shortenLocationName } from "../../../utils/helpers";
 const PopUpHoverContent = ({ feature }) => {
   return (
     <div className="p-2 m-2">
-      <span className="fs-6">
+      <span className="fs-6 fw-bold">
         {shortenLocationName(feature.properties.location_name)}
       </span>
     </div>
