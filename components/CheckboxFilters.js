75b7c4d2f29b2413bc1bc4862e0dc0ef1a98b558
John Clary <john.clary@austintexas.gov>
2022-07-18 11:53:46 -0400

    adds a color circle to filter toggles and fixes click behavior

--- components/CheckboxFilters.js
+++ components/CheckboxFilters.js
@@ -1,19 +1,20 @@
 import Form from "react-bootstrap/Form";
+import { FaCircle } from "react-icons/fa";
+
+const FilterLabel = ({ color, label }) => (
+  <div className="d-flex align-items-center">
+    <span className="me-1">{label}</span>
+    <span style={{ color: color }}>
+      <FaCircle />
+    </span>
+  </div>
+);
 
 export default function CheckboxFilters({ filters, setFilters }) {
   const onChange = (filter) => {
     const currentFilters = [...filters];
     const thisFilter = currentFilters.find((f) => f.key == filter.key);
     thisFilter.checked = !thisFilter.checked;
-
-    // // force all checkboxes to be checked if none are. prevents user from enabling all, resulting in a blank map
-    // if (
-    //   currentFilters.checkbox.every((f) => !f.checked)
-    // ) {
-    //   currentFilters.checkbox.forEach((f) => {
-    //     f.checked = true;
-    //   });
-    // }
     setFilters(currentFilters);
   };
 
@@ -22,16 +23,15 @@ export default function CheckboxFilters({ filters, setFilters }) {
       {filters.map((filter) => (
         <div
           key={filter.key}
+          className="d-flex flex-column align-items-center flex-shrink-0 border-bottom py-2 filter-toggle-wrapper "
           style={{ cursor: "pointer" }}
           onClick={() => onChange(filter)}
         >
           <Form.Check
-            type="switch"
             id={filter.key}
-            label={filter.label}
+            label={<FilterLabel {...filter} />}
             checked={filter.checked}
-            onChange={() => onChange(filter)}
-            className="text-primary"
+            style={{ cursor: "pointer", pointerEvents: "none" }}
           />
         </div>
       ))}
