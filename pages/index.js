c9daf297bbf749079680bc02f9a54c1e7bc9e42d
John Clary <john.clary@austintexas.gov>
2022-08-05 16:10:46 -0400

    add sd23 card, update tile images

--- pages/index.js
+++ pages/index.js
@@ -14,14 +14,14 @@ const cards = [
       "View crash data by month, year, mode, demographics, time of day, and location",
     href: "https://visionzero.austin.gov/viewer/",
     img: { src: "/assets/vision-zero-viewer.jpg", alt: "Vision Zero Viewer" },
-    external: true
+    external: true,
   },
   {
     title: "Bicycle Map",
     description: "Our citywide map of bicycle routes",
     href: "https://www.arcgis.com/apps/webappviewer/index.html?id=c7fecf32a2d946fabdf062285d58d40c",
     img: { src: "/assets/bicycle-map.jpg", alt: "Bicycle map" },
-    external: true
+    external: true,
   },
   {
     title: "Traffic Cameras",
@@ -41,7 +41,7 @@ const cards = [
     img: {
       src: "/assets/shared-mobility.png",
       alt: "Shared mobility dashboard",
-      external: true
+      external: true,
     },
   },
   {
@@ -49,7 +49,7 @@ const cards = [
     description: "Our signal evaluation pipeline",
     href: "/signal-evaluations",
     key: "signal_evaluations",
-    img: { src: "/assets/bicycle-map.jpg", alt: "Bicycle map" },
+    img: { src: "/assets/phb.jpg", alt: "Bicycle map" },
   },
   {
     title: "Signal Monitor",
@@ -60,17 +60,22 @@ const cards = [
   },
   {
     title: "Signal Assets",
-    description:
-      "Comprehensive map of our traffic and pedestrian signals",
+    description: "Comprehensive map of our traffic and pedestrian signals",
     href: "https://austin.maps.arcgis.com/apps/webappviewer/index.html?id=c9bda776b1934e439285570b29d3259c",
     img: { src: "/assets/signal-assets.jpg", alt: "Signal asset map" },
-    external: true
+    external: true,
+  },
+  {
+    title: "Strategic performance dashboard",
+    description: "Key performance indicators toward our strategic goals",
+    href: "https://data.austintexas.gov/stories/s/Mobility-Dashboard/gzb5-ykym/",
+    img: { src: "/assets/strategic-direction.png", alt: "Strategic direction" },
+    external: true,
   },
 
   {
     title: "Data Catalog",
-    description:
-      "Browse and download the data that powers our operations",
+    description: "Browse and download the data that powers our operations",
     href: "/data-catalog",
     img: { src: "/assets/bicycle-map.jpg", alt: "Bicycle map" },
   },
