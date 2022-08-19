900e71f2485bc56b8902d2c8072913213a5b5fa6
John Clary <john.clary@austintexas.gov>
2022-08-19 11:42:14 -0400

    simplify flex markup

--- components/pages/signal-evaluations/ListItemContent.js
+++ components/pages/signal-evaluations/ListItemContent.js
@@ -6,21 +6,17 @@ export default function ListItemContent({ feature }) {
   const settings = getSettings(feature);
   const Icon = settings.icon;
   return (
-    <div className="d-flex w-100 align-items-center justify-content-between">
+    <div className="d-flex w-100 justify-content-between">
       <span>
         <strong className="mb-1">
           {shortenLocationName(feature.properties.location_name)}
         </strong>
       </span>
-      <div className="d-flex flex-column align-items-center flex-shrink-0">
-        <div className="d-flex align-items-center">
-          <small>
-            <span className="me-1 text-muted">{settings.label}</span>
-            <span className="me-1 text-muted">
-              <Icon />
-            </span>
-          </small>
-        </div>
+      <div className="d-flex align-items-center flex-shrink-0  text-muted">
+        <span className="me-1" style={{ fontSize: "0.8em" }}>
+          {settings.label}
+        </span>
+        <Icon style={{ fontSize: "0.8em" }} />
       </div>
     </div>
   );
