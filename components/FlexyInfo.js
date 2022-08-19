190ffa2599da9c75e46259cf67f991fc2ab2b84d
John Clary <john.clary@austintexas.gov>
2022-08-19 12:38:52 -0400

    mute flexyinfo value

--- components/FlexyInfo.js
+++ components/FlexyInfo.js
@@ -2,7 +2,7 @@ export default function FlexyInfo({ label, value }) {
   return (
     <div className="d-flex justify-content-between">
       <span className="fw-bold">{label}</span>
-      <span>{value}</span>
+      <span className="text-muted">{value}</span>
     </div>
   );
 }
