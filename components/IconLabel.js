444c5f924ae031fb4d5ffe78fdada0c503309c2e
John Clary <john.clary@austintexas.gov>
2022-08-19 11:39:02 -0400

    simplify icon label markup

--- components/IconLabel.js
+++ components/IconLabel.js
@@ -1,13 +1,11 @@
 /**
  * Provides a vertically-aligned icon with label
  */
-export default function IconLabel({ Icon, label, centered }) {
+export default function IconLabel({ Icon, label }) {
   return (
-    <div className={`d-flex flex-column ${centered && "align-items-center"}`}>
-      <div className="d-flex align-items-center">
-        <Icon />
-        <span className="ms-1">{label}</span>
-      </div>
+    <div className="d-flex align-items-center">
+      <Icon />
+      <span className="ms-1">{label}</span>
     </div>
   );
 }
