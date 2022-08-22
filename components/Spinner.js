f8f0ee6a7990e03ec54b928aa65f7874a9e77bb8
John Clary <john.clary@austintexas.gov>
2022-08-22 13:06:19 -0400

    fixes spinner horiz centering

--- components/Spinner.js
+++ components/Spinner.js
@@ -5,7 +5,10 @@ import { Spinner as BsSpinner } from "react-bootstrap";
  */
 export default function Spinner() {
   return (
-    <div style={{ position: "absolute", top: "40%", left: "45%" }}>
+    <div
+      style={{ position: "absolute", top: "40%", left: "0px", right: "0px" }}
+      className="d-flex justify-content-center"
+    >
       <BsSpinner className="text-secondary" animation="border" role="status" />
       <span className="ms-2 align-top text-dts-dark-gray">Loading...</span>
     </div>
