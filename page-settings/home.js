22e1c18122e510e5fa0f50c01d4e03402c7b81c5
John Clary <john.clary@austintexas.gov>
2022-08-24 13:32:38 -0400

    use settings module for homepage

---
+++ page-settings/home.js
@@ -0,0 +1,77 @@
+// image size: 960 x 491px
+const PAGES = [
+  {
+    title: "Vision Zero Viewer",
+    description:
+      "View crash data by month, year, mode, demographics, time of day, and location",
+    href: "https://visionzero.austin.gov/viewer/",
+    img: { src: "/assets/vision-zero-viewer.jpg", alt: "Vision Zero Viewer" },
+    external: true,
+  },
+  {
+    title: "Bicycle Map",
+    description: "Our citywide map of bicycle routes",
+    href: "https://www.arcgis.com/apps/webappviewer/index.html?id=c7fecf32a2d946fabdf062285d58d40c",
+    img: { src: "/assets/bicycle-map.jpg", alt: "Bicycle map" },
+    external: true,
+  },
+  {
+    title: "Traffic Cameras",
+    description: "Live images from the City's traffic cameras",
+    href: "/traffic-cameras",
+    img: {
+      src: "/assets/traffic-cameras.jpg",
+      alt: "Traffic camera thumbnail",
+    },
+    key: "traffic_cameras",
+  },
+  {
+    title: "Shared Mobility",
+    description:
+      "Explore ridership and travel patterns of shared mobility vehicles",
+    href: "https://public.ridereport.com/austin",
+    img: {
+      src: "/assets/shared-mobility.png",
+      alt: "Shared mobility dashboard",
+      external: true,
+    },
+  },
+  {
+    title: "Signal Evaluations",
+    description: "Our signal evaluation pipeline",
+    href: "/signal-evaluations",
+    key: "signal_evaluations",
+    img: { src: "/assets/phb.jpg", alt: "Traffic signal image" },
+  },
+  {
+    title: "Signal Monitor",
+    description: "Real-time monitoring of the City's traffic signals",
+    href: "/signal-monitor",
+    img: { src: "/assets/signal-monitor.jpg", alt: "Signal monitor dashboard" },
+    key: "signal_monitor",
+  },
+  {
+    title: "Signal Assets",
+    description: "Comprehensive map of our traffic and pedestrian signals",
+    href: "https://austin.maps.arcgis.com/apps/webappviewer/index.html?id=c9bda776b1934e439285570b29d3259c",
+    img: { src: "/assets/signal-assets.jpg", alt: "Signal asset map" },
+    external: true,
+  },
+  {
+    title: "Strategic Performance",
+    description: "Key performance indicators toward our strategic goals",
+    href: "https://data.austintexas.gov/stories/s/Mobility-Dashboard/gzb5-ykym/",
+    img: { src: "/assets/strategic-direction.png", alt: "Strategic direction" },
+    external: true,
+  },
+
+  {
+    title: "Open Data",
+    description: "Browse and download the data that powers our operations",
+    href: "https://data.austintexas.gov/browse?City-of-Austin_Department-=Austin+Transportation&limitTo=datasets",
+    img: { src: "/assets/open-data-logo.png", alt: "Open data logo" },
+    external: true,
+  },
+];
+
+export default PAGES;
--- pages/index.js
+++ pages/index.js
@@ -5,94 +5,20 @@ import Col from "react-bootstrap/Col";
 import Footer from "../components/Footer";
 import Nav from "../components/Nav";
 import NavTile from "../components/NavTile";
-
-// image size: 960 x 491px
-const cards = [
-  {
-    title: "Vision Zero Viewer",
-    description:
-      "View crash data by month, year, mode, demographics, time of day, and location",
-    href: "https://visionzero.austin.gov/viewer/",
-    img: { src: "/assets/vision-zero-viewer.jpg", alt: "Vision Zero Viewer" },
-    external: true,
-  },
-  {
-    title: "Bicycle Map",
-    description: "Our citywide map of bicycle routes",
-    href: "https://www.arcgis.com/apps/webappviewer/index.html?id=c7fecf32a2d946fabdf062285d58d40c",
-    img: { src: "/assets/bicycle-map.jpg", alt: "Bicycle map" },
-    external: true,
-  },
-  {
-    title: "Traffic Cameras",
-    description: "Live images from the City's traffic cameras",
-    href: "/traffic-cameras",
-    img: {
-      src: "/assets/traffic-cameras.jpg",
-      alt: "Traffic camera thumbnail",
-    },
-    key: "traffic_cameras",
-  },
-  {
-    title: "Shared Mobility",
-    description:
-      "Explore ridership and travel patterns of shared mobility vehicles",
-    href: "https://public.ridereport.com/austin",
-    img: {
-      src: "/assets/shared-mobility.png",
-      alt: "Shared mobility dashboard",
-      external: true,
-    },
-  },
-  {
-    title: "Signal Evaluations",
-    description: "Our signal evaluation pipeline",
-    href: "/signal-evaluations",
-    key: "signal_evaluations",
-    img: { src: "/assets/phb.jpg", alt: "Traffic signal image" },
-  },
-  {
-    title: "Signal Monitor",
-    description: "Real-time monitoring of the City's traffic signals",
-    href: "/signal-monitor",
-    img: { src: "/assets/signal-monitor.jpg", alt: "Signal monitor dashboard" },
-    key: "signal_monitor",
-  },
-  {
-    title: "Signal Assets",
-    description: "Comprehensive map of our traffic and pedestrian signals",
-    href: "https://austin.maps.arcgis.com/apps/webappviewer/index.html?id=c9bda776b1934e439285570b29d3259c",
-    img: { src: "/assets/signal-assets.jpg", alt: "Signal asset map" },
-    external: true,
-  },
-  {
-    title: "Strategic Performance",
-    description: "Key performance indicators toward our strategic goals",
-    href: "https://data.austintexas.gov/stories/s/Mobility-Dashboard/gzb5-ykym/",
-    img: { src: "/assets/strategic-direction.png", alt: "Strategic direction" },
-    external: true,
-  },
-
-  {
-    title: "Open Data",
-    description: "Browse and download the data that powers our operations",
-    href: "https://data.austintexas.gov/browse?City-of-Austin_Department-=Austin+Transportation&limitTo=datasets",
-    img: { src: "/assets/open-data-logo.png", alt: "Open data logo" },
-    external: true,
-  },
-];
+import PageHead from "../components/PageHead";
+import PAGES from "../page-settings/home";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Austin Transportation Data and Performance Hub</title>
-        <meta
-          property="og:title"
-          content="Austin Transportation Data and Performance Hub"
+        <PageHead
+          title="Austin Transportation Data and Performance Hub"
+          description="Dashboards and public datasets curated by the City of Austin Transportation Department"
+          pageRoute="/"
+          imageRoute=""
         />
       </Head>
-
       <div className="wrapper">
         <Nav isHome />
         <Container className="main">
@@ -110,15 +36,15 @@ export default function Home() {
             </Col>
           </Row>
           <Row className="text-dts-4 mb-4">
-            {cards.map((card) => (
+            {PAGES.map((page) => (
               <Col
-                key={card.href}
+                key={page.href}
                 xs={12}
                 md={4}
                 lg={3}
                 className="p-2 p-md-3 p-xl-4"
               >
-                <NavTile {...card} />
+                <NavTile {...page} />
               </Col>
             ))}
           </Row>
