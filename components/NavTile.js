9b1147fca199ab15862ec8a9724465994cc71b1b
John Clary <john.clary@austintexas.gov>
2022-08-29 12:07:42 -0400

    open external links in new tab

--- components/NavTile.js
+++ components/NavTile.js
@@ -7,7 +7,7 @@ const ExternalLinkTitle = ({ title }) => (
   <div className={`d-flex flex-column`}>
     <div className="d-flex align-items-center">
       <span className="me-1">{title}</span>
-      <span className="text-muted" style={{fontSize: ".75rem"}}>
+      <span className="text-muted" style={{ fontSize: ".75rem" }}>
         <FaExternalLinkAlt />
       </span>
     </div>
@@ -15,20 +15,28 @@ const ExternalLinkTitle = ({ title }) => (
 );
 
 export default function NavTile({ href, title, description, img, external }) {
+  const anchorProps = external
+    ? {
+        target: "_blank",
+        rel: "noreferrer",
+      }
+    : {};
   return (
     <>
       <Link className="text-decoration-none" href={href} passHref>
-        <Card className="h-100 nav-tile">
-          {img && <Card.Img variant="top" alt={img.alt} src={img.src} />}
-          <Card.Body className="p-3 lh-1">
-            <Card.Title className="fw-bold fs-6 text-primary">
-              {external ? <ExternalLinkTitle title={title} /> : title}
-            </Card.Title>
-            <span className="text-muted ">
-              <small>{description}</small>
-            </span>
-          </Card.Body>
-        </Card>
+        <a className="text-decoration-none" {...anchorProps}>
+          <Card className="h-100 nav-tile">
+            {img && <Card.Img variant="top" alt={img.alt} src={img.src} />}
+            <Card.Body className="p-3 lh-1">
+              <Card.Title className="fw-bold fs-6 text-primary">
+                {external ? <ExternalLinkTitle title={title} /> : title}
+              </Card.Title>
+              <span className="text-muted ">
+                <small>{description}</small>
+              </span>
+            </Card.Body>
+          </Card>
+        </a>
       </Link>
     </>
   );
