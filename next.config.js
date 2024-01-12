7593d8b27fc778e3edc2699f3bf4b14f885ee8e2
Frank Hereford <frankhereford@users.noreply.github.com>
2024-01-12 15:36:07 -0600

    disable i18n

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+  //i18n: {
+    //locales: ["en"],
+    //defaultLocale: "en",
+  //},
 };
 
 module.exports = nextConfig;
