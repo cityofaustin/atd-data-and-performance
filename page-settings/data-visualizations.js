781c614d24abb00f052d9bbf3474ef5e60dc7f41
chiaberry <analuciaby@gmail.com>
2024-01-22 14:26:35 -0600

    add data viz page and tiles

---
+++ page-settings/data-visualizations.js
@@ -0,0 +1,7 @@
+export const KNACK_HEADERS = {
+  "X-Knack-Application-Id": "64f9f02e4a14dd0027633bcd",
+  "X-Knack-REST-API-KEY": "knack",
+};
+
+export const KNACK_URL =
+  "https://api.knack.com/v1/pages/scene_229/views/view_414/records";
