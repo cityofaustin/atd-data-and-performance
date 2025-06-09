84441834b518842ab7196b7b176a4e6eff4f2688
Frank Hereford <frank@frankhereford.com>
2025-06-09 15:16:43 -0500

    linting

--- page-settings/home.js
+++ page-settings/home.js
@@ -61,7 +61,10 @@ export const PAGES = [
     title: "Real Time Traffic Incidents",
     description: "View active traffic incidents reported to 911",
     href: "https://data.austintexas.gov/stories/s/Austin-Travis-County-Traffic-Report-Page/9qfg-4swh/",
-    img: { src: "/assets/traffic-incidents.jpg", alt: "Traffic incidents image" },
+    img: {
+      src: "/assets/traffic-incidents.jpg",
+      alt: "Traffic incidents image",
+    },
     external: true,
   },
   {
